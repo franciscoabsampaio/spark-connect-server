d402fb1afb01da4852357379ff267259ec9cf603
franciscoabsampaio <franciscoabsampaio@proton.me>
2026-04-07 19:00:51 +0100

    Add: prevent remote pulls

--- tests/test_pyspark.py
+++ tests/test_pyspark.py
@@ -33,29 +33,46 @@ def wait_for_cert(cert_path, timeout=30):
 
 
 @pytest.fixture
-def image_name():
+def image():
     if os.getenv("SPARK_CONNECT_SERVER_LOCAL"):
-        return "delta-local"
-    
-    catalog = os.getenv("CATALOG", "delta")
-    catalog_version = os.getenv("CATALOG_VERSION")
-    spark_version = os.getenv("SPARK_VERSION")
-    scala_version = os.getenv("SCALA_VERSION")
-    java_version = os.getenv("JAVA_VERSION")
+        image_name = "delta-local"
+    else:
+        catalog = os.getenv("CATALOG", "delta")
+        catalog_version = os.getenv("CATALOG_VERSION")
+        spark_version = os.getenv("SPARK_VERSION")
+        scala_version = os.getenv("SCALA_VERSION")
+        java_version = os.getenv("JAVA_VERSION")
 
-    tag = f"{catalog}-{catalog_version}-spark{spark_version}-java{java_version}-scala{scala_version}"
-    return f"franciscoabsampaio/spark-connect-server:{tag}"
+        tag = f"{catalog}-{catalog_version}-spark{spark_version}-java{java_version}-scala{scala_version}"
+        image_name = f"franciscoabsampaio/spark-connect-server:{tag}"
+
+    docker_client = docker.from_env()
+
+    # Attempt to get image from local registry,
+    # and raise if it fails, preventing any pulls from remote repositories.
+    try:
+        docker_client.images.get(image_name)
+    except docker.errors.ImageNotFound:
+        raise RuntimeError(f"Image {image_name} not found locally. Build step failed.")
+
+    return image_name
+
+
+@pytest.fixture
+def container_options(image):
+    return dict(
+        image=image,
+        detach=True,
+        ports={'15002/tcp': 15002}
+    )
 
 
 @pytest.fixture(scope="function")
-def db_backend_url(image_name):
+def db_backend_url(container_options):
     docker_client = docker.from_env()
 
     container = docker_client.containers.run(
-        image=image_name,
-        pull_policy="never",  # Prevent errors where old versions are being pulled
-        detach=True,
-        ports={'15002/tcp': 15002}
+        **container_options
     )
     wait_for_log(container, message="Spark Connect server started")
     conn_url = "sc://localhost:15002"
@@ -96,7 +113,7 @@ def test_catalog_basic_write_read(db_backend_url):
 
 
 @pytest.fixture(scope="function")
-def db_backend_url_ssl(image_name, tmp_path):
+def db_backend_url_ssl(container_options, tmp_path):
     docker_client = docker.from_env()
 
     ssl_dir = tmp_path / "ssl"
@@ -105,10 +122,7 @@ def db_backend_url_ssl(image_name, tmp_path):
     ssl_dir.chmod(0o777)
 
     container = docker_client.containers.run(
-        image=image_name,
-        pull_policy="never",  # Prevent errors where old versions are being pulled
-        detach=True,
-        ports={'15002/tcp': 15002},
+        **container_options,
         environment={"USE_SSL": "true"},
         volumes={str(ssl_dir): {"bind": "/opt/ssl", "mode": "rw"}},
     )
